ca3626a0f9b3c20f27627d8d99a5b2801bf1d53b
david <barnakdavid49@gmail.com>
2024-10-26 14:09:45 +0300

    comit1

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 from flask import Flask, request, session, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
 
-app = Flask(__name__)
+app= Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'school.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
 
 # ==== MODELS =========
-
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(150), nullable=False, unique=True)
+    password = db.Column(db.String(25), nullable=False)
+    role = db.Column(db.Integer(), default=0, nullable=False)
 
 # ==== ROUTES =========
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/no-slidebar')
+def noslidebar():
+    return render_template('no-slidebar.html')
 
 
 if __name__ == '__main__':
